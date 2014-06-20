018568e9f9f40c78bf94f8a1e763d2dac6775ef1
Bruce Li <leilmyxwz@gmail.com>
2014-06-20 13:21:31 -0400

    Use rsync for deployment instead of scp

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -3,7 +3,7 @@ import WebPage.Generate.Rules
 
 config :: Configuration
 config = defaultConfiguration
-        {deployCommand = "scp -r _site/* muyuanli@timberlake.cse.buffalo.edu:~/public_html"}
+        {deployCommand = "rsync -ave ssh _site/ muyuanli@timberlake.cse.buffalo.edu:~/public_html/"}
 
 main :: IO ()
 main = hakyllWith config compileRules
