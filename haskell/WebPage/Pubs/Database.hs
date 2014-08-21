6afc909a5852a177a88a6d7150c5f6f9a40c6c7c
Bruce Li <leilmyxwz@gmail.com>
2014-08-21 18:19:18 -0400

    Added mobihoc url and google analytics

--- haskell/WebPage/Pubs/Database.hs
+++ haskell/WebPage/Pubs/Database.hs
@@ -63,6 +63,7 @@ mobihoc2014 = conference "muyuan-2014-mobihoc"
               [muyuanli, haojin, zhaoyu, sichen, leyu, shangqian, kuiren]
               "All Your Location are Belong to Us: Breaking Mobile Social Networks for Automated User Location Tracking"
               2014
+              `atURL` "http://dl.acm.org/citation.cfm?id=2632953"
               @@ mobihoc
 
 infoPoster   = poster "muyuan-2014-infoposter"
